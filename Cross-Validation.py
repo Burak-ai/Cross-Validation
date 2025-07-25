2507d56dd2daad6b79d9438f2f021856e9666ae3
Burak Ozturk <120913735+Burak-ai@users.noreply.github.com>
2025-07-25 16:13:51 +0300

    Add files via upload

---
+++ Cross-Validation.py
@@ -0,0 +1,71 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import cross_val_score
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+
+
+data = pd.read_csv("melb_data.csv")
+
+# Select subset of predictors
+cols_to_use = ['Rooms', 'Distance', 'Landsize', 'BuildingArea', 'YearBuilt']
+X = data[cols_to_use]
+
+# Select target
+y = data.Price
+print(X)
+
+
+my_pipeline = Pipeline(steps=[('preprocessor', SimpleImputer()),
+                              ('model', RandomForestRegressor(n_estimators=50,
+                                                              random_state=0))
+                             ])
+
+scores = -1 * cross_val_score(my_pipeline, X, y, cv=5,
+                               scoring='neg_mean_absolute_error')
+print("MAE scores\n", scores)
+
+print("Average MAE score (across experiments):")
+print(scores.mean())
+
+
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+train_data = pd.read_csv('train.csv', index_col='Id')
+test_data = pd.read_csv('test.csv', index_col='Id')
+
+# Remove rows with missing target, separate target from predictors
+train_data.dropna(axis=0, subset=['SalePrice'], inplace=True)
+y = train_data.SalePrice              
+train_data.drop(['SalePrice'], axis=1, inplace=True)
+
+# Select numeric columns only
+numeric_cols = [cname for cname in train_data.columns if train_data[cname].dtype in ['int64', 'float64']]
+X = train_data[numeric_cols].copy()
+X_test = test_data[numeric_cols].copy()
+
+print(X.head())
+
+
+my_pipeline = Pipeline(steps=[
+    ('preprocessor', SimpleImputer()),
+    ('model', RandomForestRegressor(n_estimators=50, random_state=0))
+])
+
+def get_score(n_estimators):
+    my_pipeline = Pipeline(steps=[
+        ('preprocessor', SimpleImputer()),
+        ('model', RandomForestRegressor(n_estimators, random_state=0))
+    ])
+    scores = -1 * cross_val_score(my_pipeline, X, y,
+                                  cv=3,
+                                  scoring='neg_mean_absolute_error')
+    return scores.mean()
+    
+
+
